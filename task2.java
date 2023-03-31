35337c734e32e217b4cc2f5fe7c73cf7a44fcd5d
HermanYarenko <119243163+HermanYarenko@users.noreply.github.com>
2023-03-31 15:13:09 +0300

    должно работать

---
+++ task2.java
@@ -0,0 +1,26 @@
+public class task2 {
+
+    public static void main(String[]args) {
+        String jsonString ="[{\"фамилия\":\"Иванов"\,\"оценка"\:\"5"\,\"Математика\"}," +
+            "{\"фамилия\":\"Петрова"\,\"оценка"\:\"4"\,\"Информатика\"}," +
+            "{\"фамилия\":\"Краснов"\,\"оценка"\:\"5"\,\"Физика\"}]";
+        parseAndPrintJson(jsonString);
+
+    }
+    private static void parseAndPrintJson(String jsonString) {
+        StringBuilder result = new StringBuilder();
+        jsonString= jsonString.substring(1, jsonString.length() -1);
+        String[] objects = jsonString.split("},");
+
+        for (String object: objects){
+            object=object.replace( "{", "" ).replace( "}", "");
+            String[] properties = object.split(",");
+
+            String surname= properties[0].split(":")[1].replace( "\"", "");
+            String grade= properties[1].split(":")[1].replace( "\"", "");
+            String subject= properties[2].split(":")[1].replace( "\"", "");
+            result.append("Студент").append(surname).append("получил").append(grade).append("по предмету").append(subject).append(".\n");
+        }
+        System.out.println(result);
+    }
+}
